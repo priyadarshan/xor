1c991319b5b16ce5ccce386973e6da5f30c91ec0
David O'Toole <dto@gnu.org>
2011-05-20 16:37:16 -0400

    example clouds works

--- xor.lisp
+++ xor.lisp
@@ -1,4 +1,4 @@
-;;; xor.lisp --- A mashup of 8-bit space exploration and combat games
+;;; xor.lisp --- simple space game
 
 ;; Copyright (C) 2010, 2011  David O'Toole
 
@@ -19,27 +19,78 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defpackage :xor 
-    (:use :ioforms :common-lisp))
-  
+  (:use :ioforms :common-lisp))
+
 (in-package :xor)
 
-(defworld whitespace :background "story")
+(setf *screen-width* 800)
+(setf *screen-height* 600)
+(setf *window-title* "XOR!")
 
-(defsprite player 
-  :image "blue-dot" 
-  :x 20 :y 45)
+(defresource 
+    (:name "nebula" :type :image :file "nebula.png")
+    (:name "cloud1" :type :image :file "cloud1.png")
+  (:name "cloud2" :type :image :file "cloud2.png")
+  (:name "cloud3" :type :image :file "cloud3.png")
+  (:name "cloud4" :type :image :file "cloud4.png")
+  (:name "flarestar" :type :image :file "flarestar.png")
+  (:name "greenstar" :type :image :file "greenstar.png")
+  (:name "bluestar" :type :image :file "bluestar.png")
+  (:name "sparkle-1" :type :image :file "sparkle-1.png")
+  (:name "sparkle-2" :type :image :file "sparkle-2.png"))
 
-(define-method initialize player ()
-  (bind-event self (:up) (move :north 5 :pixels))
-  (bind-event self (:down) (move :south 5 :pixels))
-  (bind-event self (:right) (move :east 5 :pixels))
-  (bind-event self (:left) (move :west 5 :pixels)))
+(defvar *clouds* (list "cloud1" "cloud2" "cloud3" "cloud4"))
 
-(define-method bloop player ()
-  (play-sound self "bloop"))
+(defvar *stars* (list "flarestar" "greenstar" "bluestar"))
 
-(play (new universe)
-      :world (new whitespace)
-      :player (new player))
+(defsprite cloud 
+  :image (random-choose *clouds*)
+  :direction (random-direction :not-here)
+  :x (+ 80 (random 500)) 
+  :y (+ 80 (random 500)))
+
+(define-method draw cloud ()  
+  (with-fields (x y image) self
+    (draw-image image x y)))
+
+(define-method update cloud ()
+  (move-toward self ^direction 0.05)
+  (move-toward self (random-direction :not-here)
+	       (random 0.05)))
+
+(defsprite star 
+  :image (random-choose *stars*)
+  :x (+ 80 (random 500)) 
+  :y (+ 80 (random 500)))
+
+(defun xor ()
+  (message "RUNNING XOR!")
+  (dotimes (n 20)
+    (add-block (new cloud))))
+
+
+;; Now we define the code that runs when your game starts. We define
+;; it as a function (using `defun') to be called later by IOFORMS.
+
+;; (What happens is that IOFORMS loads this file before initializing
+;; the system, which allows you to set system variables like
+;; `*screen-height*' and `*screen-width*' before the window actually
+;; opens. Once IOFORMS is fully initialized according to the
+;; parameters you set, it will look for a function with the same name
+;; as the module---in this case `xor'---and execute it, which
+;; hands control back to you.
+
+ 
+;(defworld whitespace :background "story")
+
+;; (define-method initialize dot ()
+;;   (bind-event self (:up) (move :north 5 :pixels))
+;;   (bind-event self (:down) (move :south 5 :pixels))
+;;   (bind-event self (:right) (move :east 5 :pixels))
+;;   (bind-event self (:left) (move :west 5 :pixels)))
+
+;; (play (new universe)
+;;       :world (new whitespace)
+;;       :dot (new dot))
       
-;;; example.lisp ends here
+;;; xor.lisp ends here
